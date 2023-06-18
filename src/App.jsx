a04bd34b206d4bc5ac8f0c62783748bd5c351151
Dana <126463047+hey-dane@users.noreply.github.com>
2023-06-18 03:22:39 -0700

    updated click action and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,48 @@
+import { useState } from "react";
+
 // Write your Color component here
+// only one result when clicked
+// const Color = ({ color, setSelectedColor }) => {
+//   return <div className={color} onClick={() => setSelectedColor(color)}></div>;
+// };
+
+// handleClick gives the initial action when a circle is clicked
+const Color = ({ color, setSelectedColor, selectedColor }) => {
+  const handleClick = () => {
+    setSelectedColor(color);
+  };
+
+  const circleClassName = color === selectedColor ? `${color} selected` : color;
+
+  return <div className={circleClassName} onClick={handleClick}></div>;
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color
+          color="blue"
+          setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
+        />
+        <Color
+          color="violet"
+          setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
+        />
+        <Color
+          color="orange"
+          setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
+        />
+      </div>
     </div>
   );
 };
